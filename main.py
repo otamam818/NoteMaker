fc6b6752d9fe21f620b43faed43c3ff8aa9722d0
otamam818 <otamam818@gmail.com>
2021-09-19 08:23:09 +0600

    Added functionality to make quick to-do tasks

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# .##..##...####...######..######....##...##...####...##..##..######..#####..
+# .###.##..##..##....##....##........###.###..##..##..##.##...##......##..##.
+# .##.###..##..##....##....####......##.#.##..######..####....####....#####..
+# .##..##..##..##....##....##........##...##..##..##..##.##...##......##..##.
+# .##..##...####.....##....######....##...##..##..##..##..##..######..##..##.
+
 import sys
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, 
@@ -11,6 +17,9 @@ class NoteMaker(QWidget):
     def __init__(self, parent= None):
         super().__init__(parent=parent)
 
+        # Backend
+        self.prev_text = ""
+
         # User-interface
         prompt_label = QLabel("Enter text: ")
         self.text_area = QTextEdit()
@@ -22,6 +31,38 @@ class NoteMaker(QWidget):
 
         self.setLayout(main_layout)
         self.setWindowTitle("Note Maker")
+
+    def keyReleaseEvent(self, QKeyEvent):
+        # Make sure the current text is different from the previous 
+        # and non-empty
+        curr_text = self.text_area.toPlainText()
+        valid_change = (curr_text != self.prev_text) and (len(curr_text) > 0)
+        if valid_change:
+            self.change_text()
+
+    def change_text(self):
+        # set a map of replacables and their cursor index
+        replacables = {
+            "||[]" : ["\n[   ]"+ " "*74 + "|", 3]
+        }
+
+        # Note down the cursor position and text
+        curr_widget = self.text_area
+        curr_text = curr_widget.toPlainText()
+        curr_cursor = curr_widget.textCursor()
+
+        prev_cursor_pos = curr_cursor.position()
+
+        # Find if there is anything to replace
+        for i in replacables.keys():
+            if i in curr_text:
+                curr_text = curr_text.replace(i, replacables[i][0])
+                self.text_area.setText(curr_text)
+                curr_cursor.setPosition(prev_cursor_pos + 
+                                        replacables[i][1])
+
+        # Fix the cursor position
+        self.text_area.setTextCursor(curr_cursor)
 
 def init_app() -> None:
     # Create the QMainWindow class as an application 
