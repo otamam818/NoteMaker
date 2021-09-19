20598c4cabd2ff98845d507d8dc99987f98dc298
otamam818 <otamam818@gmail.com>
2021-09-19 10:05:23 +0600

    Refactored code for adding more features

--- main.py
+++ main.py
@@ -4,13 +4,7 @@
 # .##..##..##..##....##....##........##...##..##..##..##.##...##......##..##.
 # .##..##...####.....##....######....##...##..##..##..##..##..######..##..##.
 
-import sys
-import re
-from subprocess import check_output
-
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, 
-                               QGridLayout, QTextEdit, QWidget)
+from headers import *
 
 def main():
     # Get the screen resolution
@@ -24,7 +18,7 @@ class NoteMaker(QWidget):
 
         # Backend
         self.prev_text = ""
-        x_res, y_res = scres[0]*(5/16), scres[1]*(10/27)
+        x_res, y_res = scres[0]*X_FACTOR, scres[1]*Y_FACTOR
 
         # User-interface
         prompt_label = QLabel("Enter text: ")
@@ -48,10 +42,9 @@ class NoteMaker(QWidget):
             self.change_text()
 
     def change_text(self):
-        # set a map of replacables and their cursor index
-        replacables = {
-            "||[]" : ["\n[   ]"+ " "*74 + "|", 3]
-        }
+        basic_inputs = [INP_TO_DO]
+        param_inputs = [INP_TITLE]
+        all_feature_inputs = basic_inputs + param_inputs
 
         # Note down the cursor position and text
         curr_widget = self.text_area
@@ -61,15 +54,27 @@ class NoteMaker(QWidget):
         prev_cursor_pos = curr_cursor.position()
 
         # Find if there is anything to replace
-        for i in replacables.keys():
-            if i in curr_text:
-                curr_text = curr_text.replace(i, replacables[i][0])
-                self.text_area.setText(curr_text)
-                curr_cursor.setPosition(prev_cursor_pos + 
-                                        replacables[i][1])
+        for feature_input in all_feature_inputs:
+            if feature_input in curr_text:
+                if feature_input in basic_inputs:
+                    self.basic_replace(curr_text, curr_cursor, 
+                                       prev_cursor_pos, feature_input
+                    )
+                elif feature_input in param_inputs:
+                    pass
 
         # Fix the cursor position
         self.text_area.setTextCursor(curr_cursor)
+
+    def basic_replace(self, curr_text, curr_cursor, prev_cursor_pos, feature):
+        # set a map of basic replacables and their cursor index
+        replacables = {
+            INP_TO_DO : [ACTION_TO_DO, 3]
+        }
+
+        curr_text = curr_text.replace(feature, replacables[feature][0])
+        self.text_area.setText(curr_text)
+        curr_cursor.setPosition(prev_cursor_pos + replacables[feature][1])
 
 def get_scres():
     """ Get the screen resolution """
